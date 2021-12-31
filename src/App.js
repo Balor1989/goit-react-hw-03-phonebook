d4731843c9e429c430e9689a4c47650c72a845e3
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-31 20:24:09 +0200

    adds contacts to locale storage

--- src/App.js
+++ src/App.js
@@ -15,47 +15,67 @@ class App extends Component {
     filter:''
   }
 
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts))
+    }
+  };
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parseContacts = JSON.parse(contacts)
+    if (!contacts) {
+      return
+    }
+    this.setState({ contacts: parseContacts })
+  };
+
+
+
+
   addContactCard = (values) => {
     const { contacts } = this.state
     const repeatName = contacts.find(contact => {
-          return contact.name.toLowerCase() === values.name.toLowerCase()
-         })
-          if (repeatName) {
-            Notify.warning(`${values.name} is already in contacts`)
-            return
-          } 
+      return contact.name.toLowerCase() === values.name.toLowerCase()
+    })
+    if (repeatName) {
+      Notify.warning(`${values.name} is already in contacts`)
+      return
+    }
 
-            const contact = {
-            id: shortid.generate(),
-            name: values.name,
-            number: values.number
-        }
+    const contact = {
+      id: shortid.generate(),
+      name: values.name,
+      number: values.number
+    }
 
-        this.setState(prevState => (
-            {
-                contacts: [contact, ...prevState.contacts]
-            }
-        ))
-          values.name = ''
-          values.number=''
-         Notify.success(`${values.name} is added in contacts`)
-          }
+    this.setState(prevState => (
+      {
+        contacts: [contact, ...prevState.contacts]
+      }
+    ))
+    values.name = ''
+    values.number = ''
+    Notify.success(`${values.name} is added in contacts`)
+  };
 
   deleteContactCard = (cardId) => {
     this.setState(prevState => (
-      {contacts: prevState.contacts.filter(contact => contact.id !== cardId)}
+      { contacts: prevState.contacts.filter(contact => contact.id !== cardId) }
     ))
-  }
+  };
   
 
   filterChange = (e) => {
     this.setState({ filter: e.target.value })
     console.log(e.target.value)
-  }
+  };
   
 
   render() {
-    const { contacts, filter } = this.state
+    const { contacts, filter } = this.state;
 
     const normalizedFilter = filter.toLowerCase();
     const visibleContactCards = contacts.filter( contact => contact.name.toLowerCase().includes(normalizedFilter))
@@ -65,14 +85,14 @@ class App extends Component {
         <Phonebook
           onAddContactCard={this.addContactCard} />
         <Filter onFilterChange={this.filterChange}
-                value={filter}
+          value={filter}
         />
         <Contacts contacts={visibleContactCards}
-        onDeleteContactCard={this.deleteContactCard} 
+          onDeleteContactCard={this.deleteContactCard}
         />
       </section>
     )
-  }
+  };
 
 }
 
